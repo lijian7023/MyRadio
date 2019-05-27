668e4d54ff376c2aa42f26e39a7ed57b1ce4c93e
Liangsimin <320366897@qq.com>
2019-05-27 12:29:57 +0800

    完成MainActivity布局

--- app/src/main/java/net/lzzy/myradio/activities/MainActivity.java
+++ app/src/main/java/net/lzzy/myradio/activities/MainActivity.java
@@ -1,15 +1,26 @@
 package net.lzzy.myradio.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import net.lzzy.myradio.R;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
